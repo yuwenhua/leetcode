b90bfe8184498695dc50d373c2a40308ce849944
yuwh <yuwh.net@gmail.com>
2015-10-21 15:28:05 +0800

    modified:   rotate_array.c accepted

--- algorithms/rotate_array.c
+++ algorithms/rotate_array.c
@@ -9,7 +9,9 @@ void rotate(int* nums, int numsSize, int k) {
 
 	tmp_nums = malloc(numsSize * sizeof(int));
 	
+	
 	k = k % numsSize;
+	printf("k = %d\n", k);
 	if(! nums || numsSize <= 0) {
 		return;
 	}
@@ -17,25 +19,28 @@ void rotate(int* nums, int numsSize, int k) {
 	for(i = 0; i < numsSize; i++) {
 		tmp_nums[i] = nums[i];
 	}
+
 	for(i = 0; i < numsSize; i ++) {
-		if(i <= k) {
-			nums[i] = tmp_nums[numsSize-k+i];
+		if(i < k) {
+			nums[i] = tmp_nums[i + numsSize - k];
 		}
 		else {
-			nums[i] = tmp_nums[i+k];
+			nums[i] = tmp_nums[i-k];
 		}
 	}
+	free(tmp_nums);
 }
 
 int main(int argc, char **argv) 
 {
-	int num0[] = {1,2,3,4,5,6,7,8,9,0};
+	int num0[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int i, j;
 
 	for(i = 0; i < 20; i++) {
 		rotate(num0, 10, i);
 		for(j = 0; j < 10; j++) {
 			printf("%d ", num0[j]);
+			num0[j] = j;
 		}
 		printf("\n");
 	}
