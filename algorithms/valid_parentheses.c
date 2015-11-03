206c0d062563ad3d27a1ba28e4b1f0aa6f528f39
yuwh <yuwh.net@gmail.com>
2015-11-03 19:55:02 +0800

    parse from the most inner

--- algorithms/valid_parentheses.c
+++ algorithms/valid_parentheses.c
@@ -9,6 +9,85 @@
  * https://leetcode.com/problems/valid-parentheses/
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
 bool isValid(char* s) {
+	int len;
+	int i,j,k;
+	char *str;
+
+	if(!s)
+		return false;
+	len = strlen(s);
+	if(len % 2 == 1 || len == 0)
+		return false;
+	str = (char *)calloc(len, sizeof(char));
+	strcpy(str, s);
+
+	for(k = 0; k < len; k++) {
+		for(i = 0; i < len; i++) {
+			if(str[i] == '(') {
+				for(j = i+1; j < len; j++) {
+					if(str[j] == 0)
+						j++;
+					else 
+						break;
+				}
+				if(str[j] == ')') {
+					str[i] = 0;
+					str[j] = 0;
+				}
+			}
+
+			if(str[i] == '[') {
+				for(j = i+1; j < len; j++) {
+					if(str[j] == 0)
+						j++;
+					else 
+						break;
+				}
+				if(str[j] == ']') {
+					str[i] = 0;
+					str[j] = 0;
+				}
+			}
+
+			if(str[i] == '{') {
+				for(j = i+1; j < len; j++) {
+					if(str[j] == 0)
+						j++;
+					else 
+						break;
+				}
+				if(str[j] == '}') {
+					str[i] = 0;
+					str[j] = 0;
+				}
+			}
+		}
+	}
+	for(i = 0; i < len; i++) {
+		if(str[i] != 0)
+			return false;
+	}
+
+	return true;
+
 	    
 }
+
+int main(int argc, char **argv)
+{
+	char *str = "(([[]})){}";
+	bool result;
+
+	result = isValid(str);
+
+	printf("Is %s  valid? %d\n", str, result);
+	return 0;
+}
+
