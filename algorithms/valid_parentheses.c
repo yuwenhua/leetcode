24a6556841aa751ad774932e3c9bee46df8c3c47
yuwh <yuwh.net@gmail.com>
2015-11-03 11:52:32 +0800

    valid parentheses

---
+++ algorithms/valid_parentheses.c
@@ -0,0 +1,14 @@
+/*
+ * Given a string containing just the characters 
+ * '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.
+ *
+ * The brackets must close in the correct order, 
+ * "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ * 
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+
+bool isValid(char* s) {
+	    
+}
