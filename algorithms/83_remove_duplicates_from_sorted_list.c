0ae14069f596cad050cad8583b2b4617ec92892f
yuwh <yuwh.net@gmail.com>
2015-11-26 20:04:42 +0800

    alloc no new space

--- algorithms/83_remove_duplicates_from_sorted_list.c
+++ algorithms/83_remove_duplicates_from_sorted_list.c
@@ -55,39 +55,39 @@ void list_add_tail(struct ListNode **head, int val)
 }
 
 struct ListNode* deleteDuplicates(struct ListNode* head) {
-	int val_new;
-	int *val_old = NULL;
+	int val_new, val_old;
 	struct ListNode *p, *q;
 
 	if(!head) 
 		return NULL;
 	p = head;
+	val_old = head->val;
+	q = head->next;
+
 	while(q) {
 		val_new = q->val;
-		if(!val_old) {
-			val_old = (int *)malloc(sizeof(int));
-			*val_old = val_new;
+		if(val_old != val_new) {
+			val_old = val_new;
 			p->next = q;
 			p=q;
 		}
-		else if(*val_old != val_new) {
-			*val_old = val_new;
-			p->next = q;
-			p=q;
-		}
+
 		q = q->next;
 	}
+	p->next = NULL;
 	return head; 
 }
 int main (int argc, char **argv)
 {
 	
 	int i;
-	int val[] = {1,3,5,7,9,11,11,13,13,15,15,17,17,19,19};
+	//int val[] = {1,3,5,7,9,11,11,13,13,15,15,17,17,19,19};
+	//int val[] = {1,1,3,3,3,5,5,5,5,5}; 
+	int val[] = {1,1,1}; 
 	struct ListNode *l1 = NULL;
 	struct ListNode *new = NULL;
 
-	for(i = 0; i < 15; i++) {
+	for(i = 0; i < 3; i++) {
 		list_add_tail(&l1, val[i]);
 	}
 	
